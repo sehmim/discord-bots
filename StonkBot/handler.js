6b8e538ffafd88a72ef633818268c5ed05a57f75
sehmim2 <sehmim2010@hotmail.com>
2021-02-08 23:42:59 -0500

    adding serverless

---
+++ StonkBot/handler.js
@@ -0,0 +1,22 @@
+'use strict';
+const bot = require('index.js')
+
+module.exports.executeBot = async (event) => {
+
+
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: '',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
